9c367c143c02fcaeb8a94647a118ffe319f28ad8
DanHaggarty <danjhaggarty@gmail.com>
2025-06-13 20:28:36 +0200

    Implement RoutingWorker

--- RoutingService/Program.cs
+++ RoutingService/Program.cs
@@ -2,5 +2,7 @@ using RoutingService;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Services.AddHostedService<RoutingWorker>();
+
 var host = builder.Build();
 host.Run();
--- RoutingService/Services/RoutingWorker.cs
+++ RoutingService/Services/RoutingWorker.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoutingService.Services
+﻿namespace RoutingService
 {
-    internal class RoutingWorker
+    /// <summary>
+    /// Processes messages from routing queue.
+    /// </summary>
+    public class RoutingWorker : BackgroundService
     {
+        private readonly ILogger<RoutingWorker> _logger;
+
+        public RoutingWorker(ILogger<RoutingWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
     }
 }
